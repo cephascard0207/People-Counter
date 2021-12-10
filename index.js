f979678ce9eba935b8111bead98d603d31270047
Cephas Cardozo <cephascardozo0207@gmail.com>
2021-12-10 21:00:14 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+//Start of Passenger Counter Web App
+
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerHTML = count;
+    console.log("The Increment button was clicked!")
+    console.log(count)
+}
+
+
+
+
+
+
+
+//End of Passenger Counter Web App
+
